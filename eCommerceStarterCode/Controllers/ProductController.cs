c825a5ce20df2b0e92470a6e3a052253f19d3413
Mike Peterson <mpeterson2004@gmail.com>
2021-06-15 15:23:54 -0600

    completed product POST endpoint

---
+++ eCommerceStarterCode/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using eCommerceStarterCode.Models;
+using eCommerceStarterCode.DataTransferObjects;
+using eCommerceStarterCode.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductController>/new
+        [HttpPost("new"), Authorize]
+        public IActionResult Post([FromBody] Product newproduct)
+        {
+            _context.Products.Update(newproduct);
+            _context.SaveChanges();
+            return StatusCode(201, newproduct);
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
