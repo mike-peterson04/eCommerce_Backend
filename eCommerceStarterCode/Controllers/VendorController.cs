a1f5e4461ea151f18bcb9c937130146e3adb6e0a
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-06-15 14:44:03 -0500

    created add_vendor endpoint

---
+++ eCommerceStarterCode/Controllers/VendorController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using eCommerceStarterCode.Models;
+using eCommerceStarterCode.DataTransferObjects;
+using eCommerceStarterCode.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/vendor")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public VendorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("create"), Authorize]
+        public IActionResult Post([FromBody]Vendor newVendor)
+        {
+            _context.Vendors.Update(newVendor);
+            _context.SaveChanges();
+            return StatusCode(201, newVendor);
+        }
+    }
+}
