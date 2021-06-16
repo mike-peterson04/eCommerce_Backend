cad387d051dbee9b6a5cbfeb219a328a5b7a870a
Mike Peterson <mpeterson2004@gmail.com>
2021-06-16 13:39:00 -0600

    adding reviewController and example of generated controller

---
+++ eCommerceStarterCode/Controllers/ReviewController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceStarterCode.Models;
+using eCommerceStarterCode.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ReviewController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/<ReviewController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ReviewController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ReviewController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ReviewController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
